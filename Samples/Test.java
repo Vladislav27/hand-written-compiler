68e2b79e715e6a61a2a00feddca0b3ac2b151ba0
Komarov Mikhail <mihanya009@mail.ru>
2018-12-15 23:09:08 +0300

    Add test sample

---
+++ Samples/Test.java
@@ -0,0 +1,14 @@
+class Factorial{
+    public static void main(String[] a){
+    System.out.println(new Fac().ComputeFac());
+    }
+}
+
+class Fac {
+
+    public int ComputeFac(){
+	int num_aux;
+	num_aux = 0;
+    return num_aux;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -13,12 +13,25 @@ int main(int argc, char** argv) {
 
     Comp::CDriver driver;
     driver.parse(argv[1]);
+    // ast
 
-    NSymbolTable::SymbolTable table = NSymbolTable::BuildSymbolTable(driver.program);
+    std::ofstream outPut("./graph_ast.gv");
 
-    NTypeChecker::CheckDependencies(table);
+    NTree::GraphVizPrinterVisitor printer(outPut);
+    printer.Visit(&driver.program);
 
-    NTypeChecker::TypeCheckerVisitor checker(table);
-    checker.Visit(&driver.program);
+    outPut.close();
+
+    // irt
+    std::ofstream outIrt("./graph_irt.gv");
+
+    NSymbolTable::SymbolTable symbolTable = NSymbolTable::BuildSymbolTable(driver.program);
+
+    NIRTree::IRPrettyPrinter irPrinter(outIrt);
+    NIRTree::IRForest forest = NIRTree::BuildTree(driver.program, symbolTable);
+
+    irPrinter.Visit(forest);
+
+    outIrt.close();
     return 0;
 }
