c3ef958fe68530db48caa191767655031358deee
Vlad Mashurin <27vl@mail.ru>
2018-12-16 01:45:09 +0300

    old main

--- main.cpp
+++ main.cpp
@@ -14,11 +14,24 @@ int main(int argc, char** argv) {
     Comp::CDriver driver;
     driver.parse(argv[1]);
 
-    NSymbolTable::SymbolTable table = NSymbolTable::BuildSymbolTable(driver.program);
+    // ast
+    std::ofstream outPut("./graph_ast.gv");
 
-    NTypeChecker::CheckDependencies(table);
+    NTree::GraphVizPrinterVisitor printer(outPut);
+    printer.Visit(&driver.program);
 
-    NTypeChecker::TypeCheckerVisitor checker(table);
-    checker.Visit(&driver.program);
+    outPut.close();
+
+    // irt
+    std::ofstream outIrt("./graph_irt.gv");
+
+    NSymbolTable::SymbolTable symbolTable = NSymbolTable::BuildSymbolTable(driver.program);
+
+    NIRTree::IRPrettyPrinter irPrinter(outIrt);
+    NIRTree::IRForest forest = NIRTree::BuildTree(driver.program, symbolTable);
+
+    irPrinter.Visit(forest);
+
+    outIrt.close();
     return 0;
 }
