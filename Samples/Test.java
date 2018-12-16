1e68a3808d975d1f56f6e7237fe64b6e89e844be
Komarov Mikhail <mihanya009@mail.ru>
2018-12-16 21:49:45 +0300

    Add test java file

--- Samples/Test.java
+++ Samples/Test.java
@@ -8,7 +8,12 @@ class Fac {
 
     public int ComputeFac(){
     int a;
-    a = 0;
-    return a;
+    if (1 < 2) {
+        a = 123;
+    }
+    else{
+        a = 456;
+    }
+    return 123421;
     }
 }
